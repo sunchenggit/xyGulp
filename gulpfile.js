eff2e2183a65fde0f758a333db8baae4f243d8db
suncheng <157600408@qq.com>
2018-03-26 16:26:11 +0800

    增加js合并和压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	cssbeautify = require('gulp-cssbeautify'),
-	sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),								// 基本 gulp
+	sass = require('gulp-sass'),						// 编译sass
+	autoprefixer = require('gulp-autoprefixer'),		// css 添加前缀
+	cssbeautify = require('gulp-cssbeautify'),			// css 美化
+	sourcemaps = require('gulp-sourcemaps'),			// 资源地图
+	concat = require('gulp-concat'),					// 合并文件
+	uglify = require('gulp-uglify'),					// 压缩js
+	pump   = require('pump'),							// 
+	rename = require('gulp-rename');					// 重命名文件
 
-//源文件路径
-const url = './scss/**/*.scss';
-//输出文件路径
-const desturl = './css';
+const scssUrl = './scss/**/*.scss';			// scss文件目录
+const destCssUrl = './css';					// 编译后css文件所在目录
+const jsUrl = './js/**/*.js';				// js源文件目录
+const destJsUrl = './dist/js';				// js压缩后输出的目录
 
-/*gulp.task('default',function(){
-	//默认的代码都放在这里
-	console.log('Hello world!');
-});*/
 
+
+/* 编译scss */
 gulp.task('scss2css',function(){
-	return gulp.src(url)
+	return gulp.src(scssUrl)
 		.pipe(sourcemaps.init())
 		.pipe(sass({outputstyle:'expanded'}).on('error',sass.logError))
 		.pipe(cssbeautify({
@@ -29,6 +31,23 @@ gulp.task('scss2css',function(){
 			remove:true//去掉不必要的前缀
 		}))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(desturl))
+		.pipe(gulp.dest(destCssUrl))
 });
-gulp.watch(url,['scss2css']);
+
+/* 合并和压缩js */
+gulp.task('minjs2',function(){
+	pump([
+		gulp.src(jsUrl),
+		sourcemaps.init(),
+		concat('all.js',{
+			newLine:';'					// 合并插件的配置选项 新的一行增加 ';'
+		}),
+		rename({suffix:'.min'}),			// 重命名文件 
+		uglify(),
+		sourcemaps.write(),
+		gulp.dest(destJsUrl)
+	])
+});
+
+
+gulp.watch(scssUrl,['scss2css']);
---
+++ js/c.js
@@ -0,0 +1,3 @@
+function c(name) {
+    console.log('Hello c '+name);
+};
